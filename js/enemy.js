030f250fe33fe3ccd6d830d81b0f1effd5e014be
JulienDaCosta-GitHub <juliendacosta27@gmail.com>
2019-11-02 18:08:40 +0100

    Tuto Part 1
    Réalisation du premier tutoriel avec tous les fichiers inclus.

---
+++ js/enemy.js
@@ -0,0 +1,10 @@
+let enemy;
+
+function Enemy(enemyType, health, mana, strength, agility, speed) {
+    this.classType = enemysType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
---
+++ js/player.js
@@ -0,0 +1,10 @@
+let player;
+
+function Player(classType, health, mana, strength, agility, speed) {
+    this.classType = classType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
